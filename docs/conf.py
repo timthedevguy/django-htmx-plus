32fc81afa99a8a24e0a4abf3770133a39af662ef
Tim Davis <binary.god@gmail.com>
2026-05-16 06:26:54 -0400

    Fix commented out requirements for Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ autodoc_typehints = "description"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_logo = "_static/logo.svg"
+# html_logo = "logo.svg"
 html_theme = "shibuya"
 html_theme_options = {
     "dark_css_variables": {
